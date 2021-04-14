118174b8eadf10aff77e31bac761ea5bdb1f717c
kuangwei <xkuangwei@qq.com>
2021-04-14 10:25:27 +0800

    http demo

--- controller/article/article.go
+++ controller/article/article.go
@@ -1,7 +1,9 @@
 package article
 
 import (
+	"fmt"
 	"gin-test/model"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 
@@ -27,5 +29,33 @@ func GetArticleList(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"list":  list,
 		"total": total,
+	})
+}
+
+func MyHttp(c *gin.Context) {
+	//自定义头什么的
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://www.leiphone.com", nil)
+	req.Header.Add("auth", "test")
+	resp2, err2 := client.Do(req)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	body2, _ := ioutil.ReadAll(resp2.Body)
+	fmt.Println(body2)
+	defer resp2.Body.Close()
+
+	//普通的
+	//resp, err := http.Get("http://www.leiphone.com")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//defer resp.Body.Close()
+	//body, _ := ioutil.ReadAll(resp.Body)
+	//fmt.Printf("%V", resp)
+	c.JSON(http.StatusOK, gin.H{
+		//"body":    string(body),
+		"body2": string(body2),
+		//"content": resp.ContentLength,
 	})
 }
