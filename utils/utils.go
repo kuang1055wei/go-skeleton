2eacb1f0224938d782769b507bf4d11b1aa10948
kuangwei <xkuangwei@qq.com>
2021-04-19 17:49:14 +0800

    empty

--- utils/utils.go
+++ utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"reflect"
 	"strings"
 
 	"github.com/go-playground/validator/v10"
@@ -31,4 +32,26 @@ func EncodeMD5(value string) string {
 	m.Write([]byte(value))
 
 	return hex.EncodeToString(m.Sum(nil))
+}
+
+//判断是否为空
+func Empty(val interface{}) bool {
+	v := reflect.ValueOf(val)
+	switch v.Kind() {
+	case reflect.String, reflect.Array:
+		return v.Len() == 0
+	case reflect.Map, reflect.Slice:
+		return v.Len() == 0 || v.IsNil()
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return v.IsNil()
+	}
+	return reflect.DeepEqual(val, reflect.Zero(v.Type()).Interface())
 }
