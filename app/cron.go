01e9e9edc8e9b902fdbc1757415562b8445206ea
kuangwei <xkuangwei@qq.com>
2021-06-07 15:40:14 +0800

    计划任务加锁

--- app/cron.go
+++ app/cron.go
@@ -1,6 +1,13 @@
 package app
 
 import (
+	"context"
+	"fmt"
+	"gin-test/pkg/gredis"
+	"time"
+
+	"github.com/bsm/redislock"
+
 	"github.com/robfig/cron/v3"
 	"go.uber.org/zap"
 )
@@ -9,7 +16,7 @@ type Logger struct {
 }
 
 func startSchedule() {
-	//SkipIfStillRunning为前面任务没执行完，则跳过当前任务
+	//SkipIfStillRunning为前面任务没执行完，则跳过当前任务,分布式的时候还是需要分布式锁
 	c := cron.New(
 		cron.WithSeconds(),
 		cron.WithChain(cron.SkipIfStillRunning(cron.DefaultLogger)))
@@ -20,20 +27,37 @@ func startSchedule() {
 	//})
 
 	//加锁保证多机器只执行一次
-	//addCronFunc(c, "1/5 * * * * ?", func() {
-	//	nx, _ := gredis.GetRedis().SetNX(context.TODO(), "test_cron", 1, time.Minute).Result()
-	//	fmt.Println("锁结果", nx)
-	//	if nx {
-	//		defer func() {
-	//			fmt.Println("15s后，执行成功释放锁", nx)
-	//			gredis.GetRedis().Del(context.TODO(), "test_cron")
-	//		}()
-	//		fmt.Println("计划任务执行")
-	//	} else {
-	//		fmt.Println("----计划任务不执行----")
-	//	}
-	//	return
-	//})
+	addCronFunc(c, "* * * * * ?", func() {
+		redis := gredis.GetRedis()
+		locker := redislock.New(redis)
+		ctx := context.Background()
+		//互斥锁
+		lock, err := locker.Obtain(ctx, "my_key", time.Minute, nil)
+		if err != nil {
+			fmt.Println("----未获得锁，计划任务不执行----", err)
+			return
+		}
+		defer func() {
+			_ = lock.Release(ctx)
+		}()
+		fmt.Println("----计划任务执行---")
+		//模拟长时间执行
+		time.Sleep(time.Second * 20)
+		return
+
+		//nx, _ := gredis.GetRedis().SetNX(context.TODO(), "test_cron", 1, time.Minute).Result()
+		//fmt.Println("锁结果", nx)
+		//if nx {
+		//	defer func() {
+		//		fmt.Println("15s后，执行成功释放锁", nx)
+		//		gredis.GetRedis().Del(context.TODO(), "test_cron")
+		//	}()
+		//	fmt.Println("计划任务执行")
+		//} else {
+		//	fmt.Println("----计划任务不执行----")
+		//}
+		//return
+	})
 
 	//addCronFunc(c , "0/5 * * * * ?" , func() {
 	//	course := services.CourseExchangeCodeService
