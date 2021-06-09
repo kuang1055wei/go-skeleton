6d6b131dda3828f6f51928c78c60424a31cf981b
kuangwei <xkuangwei@qq.com>
2021-06-09 10:03:14 +0800

    db

--- pkg/simpleDb/db.go
+++ pkg/simpleDb/db.go
@@ -22,26 +22,41 @@ var (
 	sqlDB *sql.DB
 )
 
+//appMode常量
+type AppMode string
+
+const (
+	DEBUG AppMode = "debug"
+	PROD  AppMode = "prod"
+)
+
+type dbConfig struct {
+	DbUser          string
+	DbPassWord      string
+	DbHost          string
+	DbPort          string
+	DbName          string
+	MaxOpenConns    int
+	MaxIdleConns    int
+	ConnMaxIdleTime time.Duration
+	ConnMaxLifetime time.Duration
+	AppMode         AppMode //根据此变量选择日志模式
+}
+
 var err error
 
-func InitDb() error {
-	//dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-	//	config.Conf.DbUser,
-	//	config.Conf.DbPassWord,
-	//	config.Conf.DbHost,
-	//	config.Conf.DbPort,
-	//	config.Conf.DbName,
-	//)
+func dbDial(cfg *dbConfig) error {
 	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		config.Conf.DbConfig.DbUser,
-		config.Conf.DbConfig.DbPassWord,
-		config.Conf.DbConfig.DbHost,
-		config.Conf.DbConfig.DbPort,
-		config.Conf.DbConfig.DbName,
+		cfg.DbUser,
+		cfg.DbPassWord,
+		cfg.DbHost,
+		cfg.DbPort,
+		cfg.DbName,
 	)
+
 	//根据环境选择debug
 	var newLogger logger.Interface
-	if config.Conf.ServerConfig.AppMode == "debug" {
+	if cfg.AppMode == DEBUG {
 		newLogger = logger.New(
 			log.New(os.Stdout, "\r\n", log.LstdFlags),
 			logger.Config{
@@ -77,17 +92,20 @@ func InitDb() error {
 	//下面两种设置连接池的方法效果一样
 	if sqlDB, err = db.DB(); err == nil {
 		// SetMaxIdleCons 设置连接池中的最大闲置连接数。
-		sqlDB.SetMaxIdleConns(10)
+		sqlDB.SetMaxIdleConns(cfg.MaxIdleConns)
 
 		// SetMaxOpenCons 设置数据库的最大连接数量。
-		sqlDB.SetMaxOpenConns(100)
+		sqlDB.SetMaxOpenConns(cfg.MaxOpenConns)
 
 		// SetConnMaxLifetiment 设置连接的最大可复用时间。
-		sqlDB.SetConnMaxLifetime(24 * time.Hour)
+		sqlDB.SetConnMaxLifetime(cfg.ConnMaxLifetime)
+
+		//最大空闲时间
+		sqlDB.SetConnMaxIdleTime(cfg.ConnMaxIdleTime)
+
 	} else {
 		zap.L().Warn("mysql conns error:", zap.String("error", err.Error()))
 	}
-
 	//这里是多数据源的连接池
 	//db.Use(
 	//	dbresolver.Register(dbresolver.Config{}).
@@ -98,6 +116,23 @@ func InitDb() error {
 	//)
 
 	return nil
+}
+
+func InitDb() error {
+	cfg := &dbConfig{
+		DbUser:          config.Conf.DbConfig.DbUser,
+		DbPassWord:      config.Conf.DbConfig.DbPassWord,
+		DbHost:          config.Conf.DbConfig.DbHost,
+		DbPort:          config.Conf.DbConfig.DbPort,
+		DbName:          config.Conf.DbConfig.DbName,
+		MaxOpenConns:    10,
+		MaxIdleConns:    100,
+		ConnMaxIdleTime: 24 * time.Hour,
+		ConnMaxLifetime: 24 * time.Hour,
+		AppMode:         AppMode(config.Conf.AppMode),
+	}
+
+	return dbDial(cfg)
 }
 
 // 获取数据库链接
