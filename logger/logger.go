0a84196d67ad31e6d514d982122184bb2b06170c
kuangwei <xkuangwei@qq.com>
2021-06-30 15:26:05 +0800

    环境判断

--- logger/logger.go
+++ logger/logger.go
@@ -20,26 +20,31 @@ import (
 var lg *zap.Logger
 
 // InitLogger 初始化Logger
-//func InitLogger() (err error) {
-//	writeSyncer := getLogWriter(config.Conf.Filename, config.Conf.MaxSize, config.Conf.MaxBackups, config.Conf.MaxAge)
-//	encoder := getEncoder()
-//	var l = new(zapcore.Level)
-//	err = l.UnmarshalText([]byte(config.Conf.Level))
-//	if err != nil {
-//		return
-//	}
-//	core := zapcore.NewCore(encoder, writeSyncer, l)
-//
-//	lg = zap.New(core, zap.AddCaller())
-//	zap.ReplaceGlobals(lg) // 替换zap包中全局的logger实例，后续在其他包中只需使用zap.L()调用即可
-//	return
-//}
-
 func InitLogger() (err error) {
-	lg, _ = zap.NewDevelopment()
+	if config.Conf.AppMode == "debug" {
+		lg, _ = zap.NewDevelopment()
+		zap.ReplaceGlobals(lg) // 替换zap包中全局的logger实例，后续在其他包中只需使用zap.L()调用即可
+		return
+	}
+	writeSyncer := getLogWriter(config.Conf.Filename, config.Conf.MaxSize, config.Conf.MaxBackups, config.Conf.MaxAge)
+	encoder := getEncoder()
+	var l = new(zapcore.Level)
+	err = l.UnmarshalText([]byte(config.Conf.Level))
+	if err != nil {
+		return
+	}
+	core := zapcore.NewCore(encoder, writeSyncer, l)
+
+	lg = zap.New(core, zap.AddCaller())
 	zap.ReplaceGlobals(lg) // 替换zap包中全局的logger实例，后续在其他包中只需使用zap.L()调用即可
 	return
 }
+
+//func InitLogger() (err error) {
+//	lg, _ = zap.NewDevelopment()
+//	zap.ReplaceGlobals(lg) // 替换zap包中全局的logger实例，后续在其他包中只需使用zap.L()调用即可
+//	return
+//}
 
 func getLogFilePath() string {
 	return fmt.Sprintf("%s%s", config.Conf.AppConfig.RuntimeRootPath, config.Conf.LogConfig.LogSavePath)
